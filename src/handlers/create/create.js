098fa644276a6bea21e65a8c97131fa36329649c
Quentin Dejean <quentin.dejean@amaysim.com.au>
2020-03-29 16:55:07 +1100

    task/lambda-create-user: Add httpResponse handler

--- src/handlers/create/create.js
+++ src/handlers/create/create.js
@@ -1,33 +1,21 @@
 const datasource = require('../../datasources/user')
 const factory = require('./factory')
+const httpResponse = require('../../libs/httpResponse')
 
-const create = (userDataSource, userFactory) => async (event) => {
+const create = (userDataSource, userFactory, response) => async (event) => {
   const payload = JSON.parse(event.body)
-
-  let response
 
   try {
     const user = await userFactory(payload)
     await userDataSource.create(user)
 
-    response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'User created sucessfully',
-        userId: user.id,
-      }),
-    }
-  } catch (e) {
-    response = {
-      statusCode: 500,
-      body: JSON.stringify({
-        statusCode: 500,
-        message: 'An error occured while creating the user',
-      }),
-    }
+    return response.success({
+      message: 'User created sucessfully',
+      userId: user.id,
+    })
+  } catch (error) {
+    return response.error(error.message, error.statusCode)
   }
-
-  return response
 }
 
-module.exports.handler = create(datasource, factory)
+module.exports.handler = create(datasource, factory, httpResponse)
