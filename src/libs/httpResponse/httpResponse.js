0db74b58a5837ea49fa673f4e5543d8b3f3fd866
Quentin Dejean <quentin.dejean@amaysim.com.au>
2020-03-30 09:04:55 +1100

    task/json-format: Format responses as per JSON standard

--- src/libs/httpResponse/httpResponse.js
+++ src/libs/httpResponse/httpResponse.js
@@ -1,11 +1,14 @@
 const httpResponse = {
   success: (payload) => ({
     statusCode: 200,
-    body: JSON.stringify(payload),
+    data: JSON.stringify(payload),
   }),
   error: (code, message) => ({
     statusCode: code || 500,
-    message: message || 'An unknown error occured',
+    errors: {
+      status: code || 500,
+      detail: message || 'An unknown error occured',
+    },
   }),
 }
 
